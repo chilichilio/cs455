3f905bb04f2f2b7b0fd2e4e20d255574ec508a54
chilichilio <100802996+chilichilio@users.noreply.github.com>
2022-06-01 22:26:06 -0700

    Delete practice directory

--- practice/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
- // if(n>=0) {
-    //   if(0.5*(estimate+n/estimate)==estimate) return estimate;
-    // else{
-    //   return mySqrt(n,0.5*(estimate+n/estimate));
-    // } 
-    // }
-    // else return 0;
